f8734ac5a3f0e2fccdd2d92ea7f8786bb147e235
Ruslan Khamidullin <ruslan.khamidullin@toptal.com>
2021-03-25 00:50:44 +0300

    [TodoListItem] added state changes

--- src/components/todo-list-item.js
+++ src/components/todo-list-item.js
@@ -3,24 +3,55 @@ import React, { Component } from 'react';
 import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
-  render() {
-    const {label, important} = this.props;
 
-    const style = {
-      color: important ? 'steelblue' : 'black',
-      fontWeight: important ? 'bold' : 'normal'
+  constructor() {
+    super();
+
+    this.state = {
+      done: false,
+      important: false
     };
+  }
+
+  onLabelClick = () => {
+    this.setState(({done}) => {
+      return {
+        done: !done
+      };
+    });
+  }
+
+  markAsImportant = () => {
+    this.setState(({important}) => {
+      return {
+        important: !important
+      };
+    });
+  }
+
+  render() {
+    const {label} = this.props;
+
+    const { done, important } = this.state;
+    let classNames = "todo-list-item";
+    if (done) {
+      classNames += ' done';
+    }
+    if (important) {
+      classNames += ' important';
+    }
 
     return (
-      <span className="todo-list-item">
+      <span className={classNames}>
         <span
-          className="todo-list-item-label"
-          style={style}>
+          className='todo-list-item-label'
+          onClick={ this.onLabelClick }>
           {label}
         </span>
 
         <button type="button"
-                className="btn btn-outline-success btn-sm float-right">
+                className="btn btn-outline-success btn-sm float-right"
+                onClick={ this.markAsImportant }>
           <i className="fa fa-exclamation" />
         </button>
 
--- src/index.js
+++ src/index.js
@@ -11,9 +11,9 @@ import './index.css';
 const App = () => {
 
   const todoData = [
-    { label: 'Drink Coffee', important: false, id: 1 },
-    { label: 'Make Awesome App', important: true, id: 2 },
-    { label: 'Have a lunch', important: false, id: 3 }
+    { label: 'Drink Coffee', id: 1 },
+    { label: 'Make Awesome App', id: 2 },
+    { label: 'Have a lunch', id: 3 }
   ];
 
   return (
