28dee9c098098a77848f7f90ba3a7ece1874486f
Ruslan Khamidullin <ruslan.khamidullin@toptal.com>
2021-03-25 18:51:06 +0300

    [Refactoring] moved components into separated folders

---
+++ src/components/app/app.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import AppHeader from './../app-header';
+import SearchPanel from './../search-panel';
+import TodoList from './../todo-list';
+import ItemStatusFilter from './../item-status-filter';
+
+import './app.css';
+
+const App = () => {
+
+  const todoData = [
+    { label: 'Drink Coffee', id: 1 },
+    { label: 'Make Awesome App', id: 2 },
+    { label: 'Have a lunch', id: 3 }
+  ];
+
+  return (
+    <div className="todo-app">
+      <AppHeader toDo={1} done={3} />
+      <div className="top-panel d-flex">
+        <SearchPanel />
+        <ItemStatusFilter />
+      </div>
+
+      <TodoList todos={todoData} />
+    </div>
+  );
+};
+
+export default App;
