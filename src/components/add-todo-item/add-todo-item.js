962d8fb877a2786ff7bc605589b3e404f08d534d
Ruslan Khamidullin <ruslan.khamidullin@toptal.com>
2021-03-26 18:29:23 +0300

    [TodoListItem] added add item input

--- src/components/add-todo-item/add-todo-item.js
+++ src/components/add-todo-item/add-todo-item.js
@@ -4,13 +4,33 @@ import './add-todo-item.css';
 
 export default class AddTodoItem extends Component {
 
-  render() {
-    const { onItemAdded } = this.props;
+  state = {
+    label: ''
+  }
 
+  onChange = (e) => {
+    this.setState({
+      label: e.target.value
+    });
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    const { onItemAdded } = this.props;
+    onItemAdded(this.state.label);
+    document.querySelector("input.js-add-item-input").value = '';
+  }
+
+  render() {
   	return(
-      <div className='add-todo-item'>
-        <button className='btn btn-outline-secondary' onClick={ () => onItemAdded('HEllo world') }>Add</button>
-      </div>
+      <form className='add-todo-form' onSubmit={ this.onSubmit }>
+        <input type='text'
+               placeholder='what need todo'
+               onChange={ this.onChange }
+               className='js-add-item-input' />
+        <button className='btn btn-outline-secondary'>Add</button>
+      </form>
   	);
   }
 }
